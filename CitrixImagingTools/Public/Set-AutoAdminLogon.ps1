46f53354ec20856d020c6a2c6162611dd14c6eb8
Sebastian Neumann <basti.megamorf@googlemail.com>
2017-07-15 19:22:47 +0200

    PSSA fix until function rewrite is complete

--- CitrixImagingTools/Public/Set-AutoAdminLogon.ps1
+++ CitrixImagingTools/Public/Set-AutoAdminLogon.ps1
@@ -7,9 +7,9 @@ The CmdLet has the follwing parameter(s) and function(s).
 -AutoLogonCount : Sets the number of times the system would reboot without asking for credentials.Default is 1.
 -Script : Provide Full path of the script for execution after server reboot. Example : c:\test\run.bat
 
-Mandatory Parameters 
--DefaultUsername 
--DefaultPassword 
+Mandatory Parameters
+-DefaultUsername
+-DefaultPassword
 
 
 .Description
@@ -27,25 +27,27 @@ Set-AutoLogon -DefaultUsername "win\admin" -DefaultPassword "password123" -Scrip
 
 #>
 
-Function Set-AutoLogon{
-
+Function Set-AutoLogon
+{
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingUserNameAndPassWordParams", "")]
+    # ToDo rewrite entire function
     [CmdletBinding()]
     Param(
-        
-        [Parameter(Mandatory=$True,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [String[]]$DefaultUsername,
 
-        [Parameter(Mandatory=$True,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [String[]]$DefaultPassword,
 
-        [Parameter(Mandatory=$False,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [AllowEmptyString()]
         [String[]]$AutoLogonCount,
 
-        [Parameter(Mandatory=$False,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [AllowEmptyString()]
         [String[]]$Script
-                
+
     )
 
     Begin
@@ -53,23 +55,23 @@ Function Set-AutoLogon{
         #Registry path declaration
         $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
         $RegROPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
-    
+
     }
-    
+
     Process
     {
 
         try
         {
             #setting registry values
-            Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String  
-            Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String  
+            Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String
+            Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String
             Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
-            if($AutoLogonCount)
+            if ($AutoLogonCount)
             {
-                
+
                 Set-ItemProperty $RegPath "AutoLogonCount" -Value "$AutoLogonCount" -type DWord
-            
+
             }
             else
             {
@@ -77,31 +79,31 @@ Function Set-AutoLogon{
                 Set-ItemProperty $RegPath "AutoLogonCount" -Value "1" -type DWord
 
             }
-            if($Script)
+            if ($Script)
             {
-                
+
                 Set-ItemProperty $RegROPath "(Default)" -Value "$Script" -type String
-            
+
             }
             else
             {
-            
+
                 Set-ItemProperty $RegROPath "(Default)" -Value "" -type String
-            
-            }        
+
+            }
         }
 
         catch
         {
 
             Write-Output "An error had occured $Error"
-            
+
         }
     }
-    
+
     End
     {
-        
+
         #End
 
     }
