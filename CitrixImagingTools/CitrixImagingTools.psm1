58d98a25bdfa94af09203ed20317e140754ef732
Sebastian Neumann <basti.megamorf@googlemail.com>
2017-07-15 19:17:54 +0200

    Clean up module script

--- CitrixImagingTools/CitrixImagingTools.psm1
+++ CitrixImagingTools/CitrixImagingTools.psm1
@@ -1,20 +1,19 @@
 ﻿
 #Get public and private function definition files.
-    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-    $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
-    $ModuleRoot = $PSScriptRoot
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-    Foreach($import in @($Public + $Private))
+Foreach ($import in @($Public + $Private))
+{
+    Try
     {
-        Try
-        {
-            . $import.FullName
-        }
-        Catch
-        {
-            Write-Error "Failed to import function $($import.FullName): $_"
-        }
+        . $import.FullName
     }
+    Catch
+    {
+        Write-Error "Failed to import function $($import.FullName): $_"
+    }
+}
 
 Export-ModuleMember -Function $Public.Basename
